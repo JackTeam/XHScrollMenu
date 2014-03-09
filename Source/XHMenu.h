24729beb27a7d4524a05460611bd55751ab46607
xhzengAIB <xhzengAIB@gmail.com>
2014-03-09 01:14:48 +0000

    Add XHMenu class

---
+++ Source/XHMenu.h
@@ -0,0 +1,13 @@
+//
+//  XHMenu.h
+//  XHScrollMenu
+//
+//  Created by 曾 宪华 on 14-3-9.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XHMenu : NSObject
+
+@end
---
+++ Source/XHMenu.m
@@ -0,0 +1,13 @@
+//
+//  XHMenu.m
+//  XHScrollMenu
+//
+//  Created by 曾 宪华 on 14-3-9.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import "XHMenu.h"
+
+@implementation XHMenu
+
+@end
