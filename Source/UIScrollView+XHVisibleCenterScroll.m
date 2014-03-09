3849d4a6baddfd80e4c8dec002de64f227611883
xhzengAIB <xhzengAIB@gmail.com>
2014-03-09 01:16:09 +0000

    implementation UIScrollView (XHVisibleCenterScroll) logic

--- Source/UIScrollView+XHVisibleCenterScroll.m
+++ Source/UIScrollView+XHVisibleCenterScroll.m
@@ -10,4 +10,14 @@
 
 @implementation UIScrollView (XHVisibleCenterScroll)
 
+- (void)scrollRectToVisibleCenteredOn:(CGRect)visibleRect
+                             animated:(BOOL)animated {
+    CGRect centeredRect = CGRectMake(visibleRect.origin.x + visibleRect.size.width/2.0 - self.frame.size.width/2.0,
+                                     visibleRect.origin.y + visibleRect.size.height/2.0 - self.frame.size.height/2.0,
+                                     self.frame.size.width,
+                                     self.frame.size.height);
+    [self scrollRectToVisible:centeredRect
+                     animated:animated];
+}
+
 @end
