a7dab1f131c9565cc0f7f4c9f08656d0a32bbdc0
xhzengAIB <xhzengAIB@gmail.com>
2014-03-09 01:12:24 +0000

    Change struct and add XHScrollMenu class

---
+++ Source/XHScrollMenu.m
@@ -0,0 +1,31 @@
+//
+//  XHScrollMenu.m
+//  XHScrollMenu
+//
+//  Created by 曾 宪华 on 14-3-9.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import "XHScrollMenu.h"
+
+@implementation XHScrollMenu
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
