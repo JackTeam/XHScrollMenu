13231fa5f6bc6ee4f71250a6e7a57b236d7fc4e2
xhzengAIB <xhzengAIB@gmail.com>
2014-03-09 01:14:23 +0000

    implementation XHIndicatorView logic

--- Source/XHIndicatorView.m
+++ Source/XHIndicatorView.m
@@ -2,7 +2,7 @@
 //  XHIndicatorView.m
 //  XHScrollMenu
 //
-//  Created by 曾 宪华 on 14-3-9.
+//  Created by 曾 宪华 on 14-3-8.
 //  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
 //
 
@@ -10,22 +10,34 @@
 
 @implementation XHIndicatorView
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (void)setIndicatorWidth:(CGFloat)indicatorWidth {
+    _indicatorWidth = indicatorWidth;
+    CGRect indicatorRect = self.frame;
+    indicatorRect.size.width = _indicatorWidth;
+    self.frame = indicatorRect;
+}
+
++ (instancetype)initIndicatorView {
+    XHIndicatorView *indicatorView = [[XHIndicatorView alloc] initWithFrame:CGRectMake(0, 0, 50, kXHIndicatorViewHeight)];
+    return indicatorView;
+}
+
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        self.backgroundColor = [UIColor colorWithRed:0.752 green:0.026 blue:0.034 alpha:1.000];
     }
     return self;
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
 @end
