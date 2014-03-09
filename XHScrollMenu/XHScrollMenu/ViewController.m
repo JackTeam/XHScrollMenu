f035fb1c70596ff7c2a5614fef08a1111726145f
xhzengAIB <xhzengAIB@gmail.com>
2014-03-09 01:08:08 +0000

    Add project

---
+++ XHScrollMenu/XHScrollMenu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XHScrollMenu
+//
+//  Created by 曾 宪华 on 14-3-9.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
