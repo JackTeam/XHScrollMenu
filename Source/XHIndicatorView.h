b2335e9cd2fc142133c41af16192a38d5b88946a
xhzengAIB <xhzengAIB@gmail.com>
2014-03-09 01:14:06 +0000

    Design XHIndicatorView api

--- Source/XHIndicatorView.h
+++ Source/XHIndicatorView.h
@@ -2,12 +2,18 @@
 //  XHIndicatorView.h
 //  XHScrollMenu
 //
-//  Created by 曾 宪华 on 14-3-9.
+//  Created by 曾 宪华 on 14-3-8.
 //  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+#define kXHIndicatorViewHeight 3
+
 @interface XHIndicatorView : UIView
+
++ (instancetype)initIndicatorView;
+
+@property (nonatomic, assign) CGFloat indicatorWidth;
 
 @end
